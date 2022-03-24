9737b247c871658773c56222bb4d6926889d6604
4хмед57 <88539445+4hm3d57@users.noreply.github.com>
2022-03-24 18:22:30 +0300

    Update watermelon.cpp

--- watermelon.cpp
+++ watermelon.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 using namespace std;
-#define ll long long
 
 int main() {
-  int t;
-  cin >> t;
-  for(int i=0; i<t; i++) {
-    int n, s;
-    cin >> n >> s;
-    cout << s / (n*n) << "\n";
+  int w;
+  cin >> w;
+  if(w>2 && w%2==0) {
+    cout << "YES" << "\n;
+  }
+  else {
+    cout << "NO" << "\n";
   }
   return 0;
 }
+
